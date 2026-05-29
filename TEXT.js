f34e4aa284347e1816ce1e6caf70c3de9c51e27d
xcircle-ui <conpot13@duck.com>
2026-05-29 11:49:11 +0100

    Update TEXT.js

--- TEXT.js
+++ TEXT.js
@@ -1,12 +1,43 @@
-const FUNKYYYY = "★ TEST TEXT 1 ★ TEST TEXT 2 ★ TEST TEXT 3 ★ TEST TEXT 4 ★ TEST TEXT 5 ★";
+const mostlyevents = [
+  "★ this is a amazing test! ★",
+  "★ boneyard test 101 ★",
+  "★ boo ★",
+  "★ hatsune miku ★" 
+]; 
+const someweirdinfoidk = [
+  "this is second text ★",
+  "elon musk ★",
+  "jesus christ ★",
+  "sick ahh text test ★" 
+]; 
+const machineissuesorserverissues = [
+  "no maimai 3: ★",
+  "no hatsune miku project diva 3: ★",
+  "pinball machine broken. stuck ★",
+  "someotherissueidk ★"
+]; 
+const idkwhattotypehere = [
+  "jwnwjwuwb ★",
+  "jsjsbsisnsj ★",
+  "hshsbsjshshsj ★",
+  "jsjsjsjsjwjwiuwhsb ★" 
+]; const epicquoteorsomethin = [
+  "CHECK IT OUT ★",
+  "DON’T MISS IT ★",
+  "ENTER NOW ★",
+  "AVAILABLE TODAY ★"
+]; 
+function pick(list) {
+  
+  return list[Math.floor(Math.random() * list.length)];
 
-function setMarqueeText(text) {
-  const el = document.getElementById("marqueeText");
-  if (!el) return;
-
-  el.textContent = text;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  setMarqueeText(FUNKYYYY);
-});
+const el = document.getElementById("marqueeText");
+
+const message =
+  pick(mostlyevents) + " " +
+  pick(someweirdinfoidk) + " " +
+  pick(machineissuesorserverissues) + " " +
+  pick(idkwhattotypehere) + " " +
+  pick(epicquoteorsomethin); el.textContent = message
